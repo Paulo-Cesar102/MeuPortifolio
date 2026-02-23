99770a388d1be5a08415bccac4127d456a146236
Paulo-Cesar102 <153522516+Paulo-Cesar102@users.noreply.github.com>
2026-02-23 11:29:47 -0300

    fé em deus que o diabo é ruim

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-
-
 import Home from "./pages/Home";
 import Projetos from "./pages/Projetos";
 import Contato from "./pages/Contato";
@@ -8,14 +6,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-
-   
+    /* O basename deve ser exatamente o nome do seu repositório no GitHub.
+       Isso faz com que o link 'paulo-cesar102.github.io/MeuPortfolio/' 
+       seja entendido como a página Home.
+    */
+    <BrowserRouter basename="/MeuPortfolio">
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projetos" element={<Projetos />} />
         <Route path="/contato" element={<Contato />} />
+        
+        {/* Dica: Rota de fallback caso o usuário digite algo errado */}
+        <Route path="*" element={<Home />} />
       </Routes>
 
     </BrowserRouter>
