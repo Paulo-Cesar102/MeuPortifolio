7f580fb2c965c47688178d6c51ed026dc0a39c7d
Paulo-Cesar102 <153522516+Paulo-Cesar102@users.noreply.github.com>
2026-02-23 11:09:22 -0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   return {
     base: command === 'build' ? '/Meu-Portfolio/' : '/',
-
+    build: {
+      outDir: 'docs',
+    },
     plugins: [
       react({
         babel: {
