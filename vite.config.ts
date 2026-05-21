74755de5dd01bcc09761a8f6e6f95760f6fa2467
Paulo-Cesar102 <153522516+Paulo-Cesar102@users.noreply.github.com>
2026-05-21 12:26:06 -0300

    build: gerando pasta docs para o deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   // Se estivermos no GitHub Pages, usamos o nome do repo, senão usamos a raiz
-  base: '/MeuPortifolio/',
+  base: './',
   build: {
     outDir: 'docs',
     emptyOutDir: true,
